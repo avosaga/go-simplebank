0f3794fd42ed66ec34ab132acd5dc65706937db1
Alvaro Sanchez <avo.saga@gmail.com>
2023-04-19 13:47:04 -0700

    Add Paseto and JWT token makers

---
+++ token/jwt_maker.go
@@ -0,0 +1,55 @@
+package token
+
+import (
+	"fmt"
+	"github.com/golang-jwt/jwt/v5"
+	"time"
+)
+
+const minSecretKeySize = 32
+
+type JWTMaker struct {
+	secretKey string
+}
+
+func (maker *JWTMaker) CreateToken(username string, duration time.Duration) (string, error) {
+	payload, err := NewPayload(username, duration)
+	if err != nil {
+		return "", err
+	}
+
+	jwtToken := jwt.NewWithClaims(jwt.SigningMethodHS256, payload)
+	return jwtToken.SignedString([]byte(maker.secretKey))
+}
+
+func (maker *JWTMaker) VerifyToken(token string) (*Payload, error) {
+	keyFunc := func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+
+		if !ok {
+			return nil, jwt.ErrSignatureInvalid
+		}
+
+		return []byte(maker.secretKey), nil
+	}
+
+	jwtToken, err := jwt.ParseWithClaims(token, &Payload{}, keyFunc)
+	if err != nil {
+		return nil, err
+	}
+
+	payload, ok := jwtToken.Claims.(*Payload)
+	if !ok {
+		return nil, jwt.ErrTokenNotValidYet
+	}
+
+	return payload, nil
+}
+
+func NewJWTMaker(secretKey string) (Maker, error) {
+	if len(secretKey) < minSecretKeySize {
+		return nil, fmt.Errorf("invalid key size: key must be at least %d characters", minSecretKeySize)
+	}
+
+	return &JWTMaker{secretKey}, nil
+}
---
+++ token/payload.go
@@ -0,0 +1,30 @@
+package token
+
+import (
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+	"time"
+)
+
+type Payload struct {
+	jwt.RegisteredClaims
+	Username string `json:"username"`
+}
+
+func (payload *Payload) Valid() bool {
+	return !time.Now().After(payload.ExpiresAt.Time)
+}
+
+func NewPayload(username string, duration time.Duration) (*Payload, error) {
+	tokenID, err := uuid.NewRandom()
+	if err != nil {
+		return nil, err
+	}
+
+	payload := &Payload{Username: username}
+	payload.ID = tokenID.String()
+	payload.IssuedAt = jwt.NewNumericDate(time.Now())
+	payload.ExpiresAt = jwt.NewNumericDate(time.Now().Add(duration))
+
+	return payload, nil
+}
